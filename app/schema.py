9a994f53024391a543adcdabcc2ccc6f72edef5b
André Moraes <andrelamounier@gmail.com>
2024-05-27 20:54:58 -0300

    adicionando atualizações

--- app/schema.py
+++ app/schema.py
@@ -1,8 +1,28 @@
-from pydantic import BaseModel, PositiveInt
+from enum import Enum
 from typing import Optional
+from pydantic import BaseModel, PositiveFloat
 
-class ProdutosSchema(BaseModel):
-    id: PositiveInt 
-    nome: str 
-    descricao: Optional[str] = None 
-    preco: float
+from pydantic import BaseModel
+
+
+# Define o Enum para categorias
+class Categoria(Enum):
+    ELETRONICO = "eletronico"
+    CURSO = "curso"
+    ALIMENTO = "alimento"
+
+
+class ProdutoSchema(BaseModel):
+    """
+    Modelo para um item de produto
+    """
+
+    id: Optional[int] = None
+    titulo: str
+    descricao: Optional[str] = None
+    preco: PositiveFloat
+    categoria: Optional[Categoria]
+    # Adiciona o campo 'categoria', que deve ser um valor do Enum 'Categoria'
+
+    class Config:
+        from_attributes = True  # Replace 'orm_mode = True' with this
